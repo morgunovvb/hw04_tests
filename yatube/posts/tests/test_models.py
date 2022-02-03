f7d1aa65795dd0e01658df9662b4003f6a9a81bc
Вячеслав <74514540+morgunovvb@users.noreply.github.com>
2022-02-03 23:23:43 +0300

    1.2

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -1,7 +1,5 @@
-from http import HTTPStatus
-
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.test import TestCase
 
 from posts.models import Group, Post
 
@@ -25,9 +23,7 @@ class PostModelTest(TestCase):
             group=cls.group
         )
 
-
     def test_models_have_correct_object_names(self):
-        """Проверяем, что у постов корректно работает __str__"""
         post = PostModelTest.post
         group = PostModelTest.group
         post_str_method = post.text[:15]
