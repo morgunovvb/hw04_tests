e86a12bacc8bcf516c6c169627978232e8e1a6b2
morgunovvb <74514540+morgunovvb@users.noreply.github.com>
2022-02-04 16:38:40 +0300

    Update test_models.py

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -1,7 +1,5 @@
-from http import HTTPStatus
-
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.test import TestCase
 
 from posts.models import Group, Post
 
